ed60fa9ba50e75c765d7052a066a607b68444c6b
Michael Pan <panmpan@gmail.com>
2020-03-21 21:22:06 +0800

    Add multi language
    Add test scene

---
+++ Assets/Script/Language/TestLanguage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MPJamPack;
+
+public class TestLanguage : MonoBehaviour
+{
+    [SerializeField]
+    LanguageData languagePack, secondLanguagePack;
+    private bool main = true;
+
+    private void Start() {
+        LanguageMgr.AssignLanguageData(languagePack);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            main = !main;
+            LanguageMgr.AssignLanguageData(main? languagePack: secondLanguagePack);
+        }
+    }
+}
