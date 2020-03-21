806fd4733f1e368582e259a78042539ceb412aea
Michael Pan <panmpan@gmail.com>
2020-03-21 21:21:17 +0800

    Add Timer
    Add testing scene

---
+++ Assets/Script/TestTimer.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using TMPro;
+using MPJamPack;
+
+
+[RequireComponent(typeof(TextMeshPro))]
+public class TestTimer : MonoBehaviour
+{
+    [SerializeField]
+    Timer updateTimer, resetTimer;
+    [SerializeField]
+    FloatLerpTimer floatLerp;
+    [SerializeField]
+    AnimationCurve curve;
+    [SerializeField]
+    Vector3LerpTimer positionLerpTimer;
+    [SerializeField]
+    ColorLerpTimer colorLerpTimer;
+
+    TextMeshPro text;
+
+    private void Awake() {
+        text = GetComponent<TextMeshPro>();
+    }
+
+
+    void Update()
+    {
+        if (positionLerpTimer.Timer.UpdateEnd) {
+            transform.position = positionLerpTimer.Value;
+            positionLerpTimer.Timer.ReverseMode = !positionLerpTimer.Timer.ReverseMode;
+        }
+        else transform.position = positionLerpTimer.Value;
+
+        if (colorLerpTimer.Timer.UpdateEnd)
+        {
+            text.color = colorLerpTimer.Value;
+            colorLerpTimer.Timer.ReverseMode = !colorLerpTimer.Timer.ReverseMode;
+        }
+        else text.color = colorLerpTimer.Value;
+
+        if (resetTimer.Running) {
+            if (resetTimer.UpdateEnd) resetTimer.Running = false;
+            return;
+        }
+
+        if (floatLerp.Timer.UpdateEnd) {
+            updateTimer.Reset();
+            resetTimer.Reset();
+            floatLerp.Timer.Reset();
+            text.text = "0";
+        }
+        else {
+            if (updateTimer.UpdateEnd)
+            {
+                text.text = floatLerp.CurvedValue(curve).ToString();
+                updateTimer.Reset();
+            }
+        }
+    }
+}
