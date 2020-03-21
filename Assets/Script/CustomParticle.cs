10babbd5aafa4129a6483c93a17f6c868f717ecb
Michael Pan <panmpan@gmail.com>
2020-03-21 22:43:43 +0800

    Add prefab pool
    Add test scene

---
+++ Assets/Script/CustomParticle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MPJamPack;
+
+public class CustomParticle : MonoBehaviour, IPoolableObj
+{
+    ParticleSpawner spawner;
+    Vector3LerpTimer positionLerp;
+
+    public void Instantiate()
+    {
+    }
+
+    public void DeactivateObj(Transform poolCollection)
+    {
+        gameObject.SetActive(false);
+
+        if (poolCollection != null) transform.SetParent(poolCollection);
+    }
+
+    public void Reinstantiate()
+    {
+        gameObject.SetActive(true);
+        transform.SetParent(null);
+    }
+
+    public void Shoot(ParticleSpawner _spawner, Vector3 from, Vector3 to, float time) {
+        spawner = _spawner;
+        positionLerp = new Vector3LerpTimer(from, to, time);
+        transform.position = from;
+    }
+
+    private void Update() {
+        if (positionLerp.Timer.UpdateEnd) {
+            spawner.ParticleShootEnd(this);
+        }
+        else {
+            transform.position = positionLerp.Value;
+        }
+    }
+}
---
+++ Assets/Script/ParticleSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MPJamPack;
+
+public class ParticleSpawner : MonoBehaviour
+{
+    PrefabPool<CustomParticle> pool;
+
+    [SerializeField]
+    CustomParticle prefab;
+
+    [SerializeField]
+    Timer spawnTimer;
+
+    private void Awake() {
+        pool = new PrefabPool<CustomParticle>(prefab, true, "ParticleCollection");
+    }
+
+    private void Update() {
+        if (spawnTimer.UpdateEnd) {
+            spawnTimer.Reset();
+
+            CustomParticle particle = pool.Get();
+            particle.Shoot(this,
+                           new Vector3(Random.Range(-5, 5), Random.Range(-3, 3)),
+                           new Vector3(Random.Range(-5, 5), Random.Range(-3, 3)),
+                           Random.Range(1, 1.5f));
+        }
+    }
+
+    public void ParticleShootEnd(CustomParticle particle) {
+        pool.Put(particle);
+    }
+}
