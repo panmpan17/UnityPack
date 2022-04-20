ee150e83f5afabf5e643ab096011584f2f515358
Michael Pan <panmpan@gmail.com>
2022-04-20 21:13:02 +0800

    Add test data

--- Assets/Script/TestAttribute.cs
+++ Assets/Script/TestAttribute.cs
@@ -10,4 +10,19 @@ public class TestAttribute : MonoBehaviour
     [Layer]
     public int layer;
     public ValueWithEnable<int> overrideInt;
+
+    [Space(10)]
+    public IntReference intReference;
+    public FloatReference floatReference;
+    public BoolReference boolReference;
+
+    [Space(10)]
+    public ColorReference colorRference;
+    public AnimationCurveReference animationCurve;
+
+    [Space(10)]
+    public IntRangeReference intRangeReference;
+    public RangeReference rangeReference;
+
+    public EventReference eventReference;
 }
